adead192fac9a96efb22db3bd13d7bc8c3895f20
Jeremy Ellis <keyfreemusic@gmail.com>
2024-03-13 09:43:36 -0700

    Update seeed-i2c-2xiao-transmitter.ino

--- seeed-sketches/seeed-i2c-2xiao-transmitter.ino
+++ seeed-sketches/seeed-i2c-2xiao-transmitter.ino
@@ -1,25 +1,37 @@
-// Instructions:
-// Connect Arduino 1's SDA to Arduino 2's SDA
-// Connect Arduino 1's SCL to Arduino 2's SCL
-// Upload seeed-i2c-2xiao-receiver.ino to Arduino 1
-// Upload seeed-i2c-2xiao-transmitter.ino to Arduino 2
-// Watch Serial Monitor on Arduino 1
+// Instructions: 
+// Connect Arduino 1's SDA to Arduino 2's SDA 
+// Connect Arduino 1's SCL to Arduino 2's SCL 
+// Upload seeed-i2c-2xiao-receiver.ino to Arduino 1 
+// Upload seeed-i2c-2xiao-transmitter.ino to Arduino 2 
+// Watch Serial Monitor on Arduino 1 
 
-#include <Wire.h>
+#include <Wire.h> 
 
-#define receiverAddress 9
+#define receiverAddress 9 
 
-int counter = 0;
+uint8_t counter = 0; 
 
-void setup() {
-  Serial.begin(115200);
-  Wire.begin();
-}
+void setup() { 
+  Serial.begin(115200); 
+  Wire.begin(); 
+} 
 
-void loop() {
-  counter++;
-  Wire.beginTransmission(receiverAddress);
-  Wire.write(counter);
-  Wire.endTransmission();
-  delay(500);
-}
+ 
+
+void loop() { 
+  counter++; 
+  if (counter > 255) { 
+    counter = 0; 
+  } 
+
+  Wire.beginTransmission(receiverAddress); 
+  Wire.write(counter); 
+  Wire.endTransmission(); 
+
+  delay(500); 
+
+} 
+
+ 
+
+ 
