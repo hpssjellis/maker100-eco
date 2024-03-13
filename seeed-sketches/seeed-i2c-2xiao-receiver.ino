cfe7e17b3b91da2108f771eb68cc6e887102aa4e
Jeremy Ellis <keyfreemusic@gmail.com>
2024-03-13 09:42:38 -0700

    Update seeed-i2c-2xiao-receiver.ino

--- seeed-sketches/seeed-i2c-2xiao-receiver.ino
+++ seeed-sketches/seeed-i2c-2xiao-receiver.ino
@@ -1,26 +1,59 @@
-// Instructions:
-// Connect Arduino 1's SDA to Arduino 2's SDA
-// Connect Arduino 1's SCL to Arduino 2's SCL
-// Upload seeed-i2c-2xiao-receiver.ino to Arduino 1
-// Upload seeed-i2c-2xiao-transmitter.ino to Arduino 2
-// Watch Serial Monitor on Arduino 1
+ 
 
-#include <Wire.h>
+// Instructions: 
 
-#define receiverAddress 9
+// Connect Arduino 1's SDA to Arduino 2's SDA 
 
-uint8_t received = 0;
-void receiveEvent(int bytes) {
-  received = Wire.read();
-}
+// Connect Arduino 1's SCL to Arduino 2's SCL 
 
-void setup() {
-  Serial.begin(115200);
-  Wire.begin(receiverAddress);
-  Wire.onReceive(receiveEvent);
-}
+// Upload seeed-i2c-2xiao-receiver.ino to Arduino 1 
 
-void loop() {
-  Serial.println(String(received));
-  delay(500);
-}
+// Upload seeed-i2c-2xiao-transmitter.ino to Arduino 2 
+
+// Watch Serial Monitor on Arduino 1 
+
+ 
+
+#include <Wire.h> 
+
+ 
+
+#define receiverAddress 9 
+
+ 
+
+uint8_t received = 0; 
+
+void receiveEvent(int bytes) { 
+
+  received = Wire.read(); 
+
+   
+
+  Serial.println(String(received)); 
+
+} 
+
+ 
+
+void setup() { 
+
+  Serial.begin(115200); 
+
+  Wire.begin(receiverAddress); 
+
+  Wire.onReceive(receiveEvent); 
+
+} 
+
+ 
+
+void loop() { 
+
+ // Serial.println(String(received)); 
+
+ // delay(500); 
+
+} 
+
+ 
