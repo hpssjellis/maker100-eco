cf3e39c9a3acf30b746a65d112a969c7c09e92da
Jeremy Ellis <keyfreemusic@gmail.com>
2024-03-14 09:23:25 -0700

    Update seeed-i2c-2xiao-transmitter.ino

--- seeed-sketches/seeed-i2c-2xiao-transmitter.ino
+++ seeed-sketches/seeed-i2c-2xiao-transmitter.ino
@@ -1,9 +1,11 @@
 // Instructions: 
-// Connect Arduino 1's SDA to Arduino 2's SDA 
-// Connect Arduino 1's SCL to Arduino 2's SCL 
+// Connect Arduino 1 receiver GND to Arduino 2 transmitter GND
+// Connect Arduino 1 receiver 3V3 to Arduino 2 transmitter 3V3 
+// Connect Arduino 1 receiver SDA to Arduino 2 transmitter SDA  (on XIAO SDA D4)
+// Connect Arduino 1 receiver SCL to Arduino 2 transmitter SCL  (on XIAO SCL D5)
 // Upload seeed-i2c-2xiao-receiver.ino to Arduino 1 
 // Upload seeed-i2c-2xiao-transmitter.ino to Arduino 2 
-// Watch Serial Monitor on Arduino 1 
+// Watch Serial Monitor on Arduino 1 receiver
 
 #include <Wire.h> 
 
