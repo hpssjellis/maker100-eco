c09af856f01dee6699905f2032897e5861c3f1c9
Jeremy Ellis <keyfreemusic@gmail.com>
2024-03-14 13:35:29 -0700

    Update seeed-i2c-2xiao-receiver.ino

--- seeed-sketches/seeed-i2c-2xiao-receiver.ino
+++ seeed-sketches/seeed-i2c-2xiao-receiver.ino
@@ -1,3 +1,6 @@
+//seeed-i2c-2xiao-receiver.ino
+
+
 // Instructions: 
 // Connect Arduino 1 receiver GND to Arduino 2 transmitter GND
 // Connect Arduino 1 receiver 3V3 to Arduino 2 transmitter 3V3 
