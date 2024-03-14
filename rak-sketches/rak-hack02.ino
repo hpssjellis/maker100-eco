4828dc987bd1589f601ef6927c5be6fc23cc8c39
Jeremy Ellis <keyfreemusic@gmail.com>
2024-03-13 23:50:25 -0700

    Create rak-hack02.ino

---
+++ rak-sketches/rak-hack02.ino
@@ -0,0 +1,139 @@
+/*************************************
+ *
+ * Rak Hack made to print all LoRaWan and LoRa P2P settings
+ *
+*************************************/
+
+void myBuffPrint(uint8_t* myBuff, uint8_t myLen){
+    for (int i = 0; i < myLen; i++) {
+      if (myBuff[i] < 0x10) {
+        Serial.print("0");
+      }
+    Serial.print(myBuff[i], HEX);
+  }
+}
+
+
+void myLoraPrint(){
+
+    Serial.println();
+    Serial.println("LoRa P2P Information RAK HACK");
+    Serial.println("------------------------------------------------------");
+    Serial.printf("AT+HWMODEL=? Hardware ID: %s\r\n",        api.system.chipId.get().c_str());
+    Serial.printf("AT+HWID=?    Model ID: %s\r\n",           api.system.modelId.get().c_str());
+    Serial.printf("AT+APIVER=?  RUI API Version: %s\r\n",    api.system.apiVersion.get().c_str());
+    Serial.printf("AT+VER=?     Firmware Version: %s\r\n",   api.system.firmwareVersion.get().c_str());
+    Serial.printf("AT+CLIVER=?  AT Command Version: %s\r\n", api.system.cliVersion.get().c_str());
+    Serial.println();
+    Serial.println("At Commands: AT? for help, ATE for echo , ATZ for reset, AT+DEBUG=1 for debug mode");
+    Serial.println("At Commands: AT+BOOT to upload code, AT+RUN another way to reset, AT+JOIN attempts to join");
+    Serial.println("At Commands: AT+P2P=?  gives most of the P2P settings");
+    Serial.println();
+    
+    Serial.printf("AT+NWM=? network working mode (0 = P2P_LORA, 1 = LoRaWAN, 2 = P2P_FSK)  %d\r\n", api.lorawan.nwm.get());
+
+
+
+
+
+
+
+    Serial.println("Switch to LoRaWan mode and resetting in 10s");
+    delay(10000);
+    api.lorawan.nwm.set(1);
+    api.system.reboot();
+}
+
+    //Serial.printf("Set P2P mode frequency  %s\r\n",api.lorawan.pfreq.get());
+  
+  /*
+    Serial.printf("Set P2P mode spreading factor %d: %s\r\n", sf,
+      api.lorawan.psf.set(sf) ? "Success" : "Fail");
+    Serial.printf("Set P2P mode bandwidth %d: %s\r\n", bw,
+      api.lorawan.pbw.set(bw) ? "Success" : "Fail");
+    Serial.printf("Set P2P mode code rate 4/%d: %s\r\n", (cr + 5),
+      api.lorawan.pcr.set(cr) ? "Success" : "Fail");
+    Serial.printf("Set P2P mode preamble length %d: %s\r\n", preamble,
+      api.lorawan.ppl.set(preamble) ? "Success" : "Fail");
+    Serial.printf("Set P2P mode tx power %d: %s\r\n", txPower,
+      api.lorawan.ptp.set(txPower) ? "Success" : "Fail");
+    api.lorawan.registerPRecvCallback(recv_cb);
+    api.lorawan.registerPSendCallback(send_cb);
+  
+*/
+
+
+
+void myLoRaWanPrint(){
+    uint8_t buff[8];
+    uint8_t buffBig[16];
+
+    Serial.println();
+    Serial.println("LoRaWan Information RAK HACK");
+    Serial.println("------------------------------------------------------");
+    Serial.printf("AT+HWMODEL=? Hardware ID: %s\r\n",        api.system.chipId.get().c_str());
+    Serial.printf("AT+HWID=?    Model ID: %s\r\n",           api.system.modelId.get().c_str());
+    Serial.printf("AT+APIVER=?  RUI API Version: %s\r\n",    api.system.apiVersion.get().c_str());
+    Serial.printf("AT+VER=?     Firmware Version: %s\r\n",   api.system.firmwareVersion.get().c_str());
+    Serial.printf("AT+CLIVER=?  AT Command Version: %s\r\n", api.system.cliVersion.get().c_str());
+    Serial.println();
+    Serial.println("At Commands: AT? for help, ATE for echo , ATZ for reset, AT+DEBUG=1 for debug mode");
+    Serial.println("At Commands: AT+BOOT to upload code, AT+RUN another way to reset, AT+JOIN attempts to join");
+
+
+    Serial.printf("AT+NWM=?  network working mode (0 = P2P_LORA, 1 = LoRaWAN, 2 = P2P_FSK)   %d\r\n", api.lorawan.nwm.get());
+
+    api.lorawan.deui.get(buff, 8);
+    Serial.print("AT+DEVEUI=? device EUI 8 bytes  ");
+    myBuffPrint(buff, 8);
+    Serial.println();
+
+    api.lorawan.appeui.get(buff, 8);
+    Serial.print("AT+APPEUI=?   application EUI 8 bytes  ");
+    myBuffPrint(buff, 8);
+    Serial.println();
+
+    api.lorawan.appkey.get(buffBig, 16);
+    Serial.print("AT+APPKEY=?  application key 16 bytes ");
+    myBuffPrint(buffBig, 16);
+    Serial.println();
+
+
+
+    Serial.printf("AT+NJM=?  network join mode (0 = ABP, 1 = OTAA)   %d\r\n", api.lorawan.njm.get());
+    Serial.printf("AT+DR=?   get the data rate %d\r\n", api.lorawan.dr.get());
+
+   
+
+
+
+
+
+
+    Serial.println("Switch to LoRa mode and resetting in 10s");
+    delay(10000);
+    api.lorawan.nwm.set(0);
+    api.system.reboot();
+
+}
+
+
+
+
+void setup(){
+    Serial.begin(115200);
+
+ 
+    if(api.lorawan.nwm.get() == 0) {    // LoRa
+       myLoraPrint();
+     } else   {
+       myLoRaWanPrint(); 
+    }
+
+
+}
+
+
+void loop(){
+
+}
