15ee9afc51b31f13c7e6dcf1994bb23151498f37
Jeremy Ellis <keyfreemusic@gmail.com>
2024-03-29 13:13:59 -0700

    Update rak-hack06.ino

--- rak-sketches/rak-hack06.ino
+++ rak-sketches/rak-hack06.ino
@@ -27,7 +27,7 @@ void myLoraPrint(){
     Serial.printf("AT+CLIVER=?  AT Command Version: %s\r\n", api.system.cliVersion.get().c_str());
     Serial.println();
     Serial.println("At Commands: AT? for help, ATE for echo , ATZ for reset, AT+DEBUG=1 for debug mode");
-    Serial.println("At Commands: AT+BOOT to upload code, AT+RUN another way to reset, AT+JOIN attempts to join");
+    Serial.println("At Commands: AT+BOOT to upload code, AT+RUN reset after boot mode");
     Serial.println("At Commands: AT+P2P=?  gives most of the P2P settings");
     Serial.println("At Commands: AT+PSEND=61624142203132      P2P send data   'abAB 12' ");    
     Serial.println("At Commands: AT+PRECV=?   Codes for P2P received data:  try 0= only send, 30000=receive for 30 s, 65533 can receive and send, 65534=wait for one receive then can send, 65535 = only receive");    
@@ -41,7 +41,7 @@ void myLoraPrint(){
     Serial.printf("AT+PSF=?     P2P mode spreading factor  5-12: %d\r\n", api.lorawan.psf.get());
     Serial.printf("AT+PBW=?     P2P mode bandwidth      (LORA: 0 = 125, 1 = 250, 2 = 500, 3 = 7.8, 4 = 10.4, 5 = 15.63, 6 = 20.83, 7 = 31.25, 8 = 41.67, 9 = 62.5 FSK: 4800-467000): %d\r\n", api.lorawan.pbw.get());
     Serial.printf("AT+PCR=?     P2P mode code rate     (0=4/5, 1=4/6, 2=4/7, 3=4/8): %d\r\n", api.lorawan.pcr.get());
-    Serial.printf("AT+PPL=?     P2P mode preamble length     (5-65535)   default = 8:         AT+PPL=8 %d\r\n", api.lorawan.ppl.get());
+    Serial.printf("AT+PPL=?     P2P mode preamble length     (5-65535)   default = 8: %d\r\n", api.lorawan.ppl.get());
   //  Serial.printf("AT+PRECV=?   Codes for P2P received data: %d\r\n", api.lorawan.precv());
     Serial.printf("AT+ENCRY=?   P2P mode encryption. 0 default no encryption, 1 encryption: %d\r\n", api.lorawan.encry.get());
     //  Serial.printf("AT+PSEND=61624142203132      P2P send data   'abAB 12' %d\r\n", api.lorawan.psend(sizeof(61624142203132),61624142203132 ));
@@ -74,7 +74,7 @@ void myLoRaWanPrint(){
     Serial.printf("AT+CLIVER=?  AT Command Version: %s\r\n", api.system.cliVersion.get().c_str());
     Serial.println();
     Serial.println("At Commands: AT? for help, ATE for echo , ATZ for reset, AT+DEBUG=1 for debug mode");
-    Serial.println("At Commands: AT+BOOT to upload code, AT+RUN another way to reset, AT+JOIN attempts to join");
+    Serial.println("At Commands: AT+BOOT to upload code, AT+RUN reset after boot mode, AT+JOIN attempts to join");
     Serial.println("At Commands: AT+SEND=12:61624142203132     send data   'abAB 12'   11 byte or less send");
     Serial.println("At Commands: AT+LPSEND=2:0:03683C0467067C07730010080200C8   long send     ");
     Serial.println();
