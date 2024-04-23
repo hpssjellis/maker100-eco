4c470483640bccf6016e0748354c33e2cb6070c6
Jeremy Ellis <keyfreemusic@gmail.com>
2024-04-23 13:42:18 -0700

    Create draft-espNow.ino

---
+++ seeed-sketches/draft-espNow.ino
@@ -0,0 +1,194 @@
+Upload MacAdress code to RECEIVER board (XAIO #1). Make sure not to get them mixed up. Check serial monitor for output MacAdress. Save it.
+Ask chatGPT to convert it into the format needed for arduino, replace empty MacAdress in SENDER code on a DIFFERENT XAIO #2.
+Upload sender code to XAIO #2
+Upload Receiver code to XAIO #1
+XAIO #2 (Sender) must be powered for it to work. Plug into a different computer or power bank.
+Plug XAIO #1 (Receiver) into a computer and check serial monitor.
+
+
+
+
+------------------------------------------------------
+
+  #include <WiFi.h>
+
+void setup() {
+  Serial.begin(115200);
+  
+  // Print MAC address
+  Serial.println("MAC Address:");
+  Serial.println(WiFi.macAddress());
+}
+
+void loop() {
+  // Do nothing in loop
+}
+
+
+
+
+
+
+
+---------------------------------
+
+
+
+
+  /*
+//board e8
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp-now-esp32-arduino-ide/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+
+#include <esp_now.h>
+#include <WiFi.h>
+int myNum = 0;
+// REPLACE WITH YOUR RECEIVER MAC Address
+uint8_t broadcastAddress[] ={//see mac address code to get your RECEIVER BOARD mac address};
+
+// Structure example to send data
+// Must match the receiver structure
+typedef struct struct_message {
+  char a[32];
+
+} struct_message;
+
+// Create a struct_message called myData
+struct_message myData;
+
+esp_now_peer_info_t peerInfo;
+
+// callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+ 
+void setup() {
+  // Init Serial Monitor
+  Serial.begin(115200);
+ 
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+  
+  // Register peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+}
+ 
+void loop() {
+  myNum++;
+  // Set values to send
+sprintf(myData.a, "Number Received: %d", myNum);
+
+  
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+   
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+  delay(2000);
+}
+
+
+
+
+
+
+
+-------------------------------------------
+
+
+
+
+
+
+
+  /*
+  Rui Santos original code 
+  Complete project details at https://RandomNerdTutorials.com/esp-now-esp32-arduino-ide/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+
+*/
+
+#include <esp_now.h>
+#include <WiFi.h>
+
+// Structure example to receive data
+// Must match the sender structure
+typedef struct struct_message {
+    char a[32];
+    int b;
+    float c;
+    bool d;
+} struct_message;
+
+// Create a struct_message called myData
+struct_message myData;
+
+// callback function that will be executed when data is received
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&myData, incomingData, sizeof(myData));
+  Serial.print("Receive Success, Bytes received: ");
+  Serial.println(len);
+  Serial.print("New Message From Delivery ESP: ");
+  Serial.println(myData.a);
+Serial.print("-----------------------------------------------------------------------------------\n");
+
+}
+ 
+void setup() {
+  // Initialize Serial Monitor
+  Serial.begin(115200);
+  
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info
+  esp_now_register_recv_cb(OnDataRecv);
+}
+ 
+void loop() {
+
+}
