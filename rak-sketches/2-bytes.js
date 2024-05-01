2a10880a1126e4b461a4aafb2809db081cd9f121
Jeremy Ellis <keyfreemusic@gmail.com>
2024-05-01 12:11:21 -0700

    Update 2-bytes.js

--- rak-sketches/2-bytes.js
+++ rak-sketches/2-bytes.js
@@ -1,1 +1,10 @@
-//we need a decoder here
+function Decoder(payload, port) {
+    if(port === 2) {
+        return [
+            {
+                field: "X",
+                value: payload[0] / 100
+            }
+        ];
+    }
+}
