e1007fee8f510bf0978298f163333c8d7ddc57c1
packetbytebuf <144747544+packetbytebuf@users.noreply.github.com>
2025-03-03 11:06:42 -0800

    Fix a26 sound to be compatible with the XIAO ESP 32 S3 boards.

--- seeed-sketches/a02-sound-to-sd-card.ino
+++ seeed-sketches/a02-sound-to-sd-card.ino
@@ -1,141 +1,76 @@
-/* 
- * WAV Recorder for Seeed XIAO ESP32S3 Sense 
+/*
+  Must enable OPI PSRAM, and USB CDC on Boot (Tools menu at top)
+
+  Most code taken from the ESP_I2S example Record_to_WAV.ino (https://github.com/Seeed-Studio/wiki-documents/blob/docusaurus-version/docs/Sensor/SeeedStudio_XIAO/SeeedStudio_XIAO_ESP32S3_Sense/XIAO_ESP32S3_Sense_mic.md)
 */
 
-#include <I2S.h>
+#include "ESP_I2S.h"
 #include "FS.h"
 #include "SD.h"
-#include "SPI.h"
 
-// make changes as needed
-#define RECORD_TIME   5  // seconds, The maximum value is 240
-//#define WAV_FILE_NAME "arduino_rec"
-
-// do not change for best
-#define SAMPLE_RATE 16000U
-#define SAMPLE_BITS 16
-#define WAV_HEADER_SIZE 44
-#define VOLUME_GAIN 2
-
-int myCount = 0;
-char myWavFileName[30];  
-char myFolderName[10] = "/Cool";
+#define RECORD_TIME 5
+#define FILE_NAME "sample.wav"
 
 void setup() {
-  randomSeed(analogRead(0));  // Seed the random number generator
-  myCount = random(100000);
+  // Create an instance of the I2SClass
+  I2SClass i2s;
+
+  // Create variables to store the audio data
+  uint8_t *wav_buffer;
+  size_t wav_size;
+
+  // Initialize the serial port
   Serial.begin(115200);
-  pinMode(LED_BUILTIN, OUTPUT);
-  //while (!Serial) ;
-  delay(10000);  //just wait some seconds;
-  I2S.setAllPins(-1, 42, 41, -1, -1);
-  if (!I2S.begin(PDM_MONO_MODE, SAMPLE_RATE, SAMPLE_BITS)) {
-    Serial.println("Failed to initialize I2S!");
-    while (1) ;
+  while (!Serial) {
+    delay(10);
   }
+
+  Serial.println("Initializing I2S bus...");
+
+  // Set up the pins used for audio input
+  i2s.setPinsPdmRx(42, 41);
+
+  // start I2S at 16 kHz with 16-bits per sample
+  if (!i2s.begin(I2S_MODE_PDM_RX, 16000, I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_MONO)) {
+    Serial.println("Failed to initialize I2S!");
+    while (1); // do nothing
+  }
+
+  Serial.println("I2S bus initialized.");
+  Serial.println("Initializing SD card...");
+
+  // Set up the pins used for SD card access
   if(!SD.begin(21)){
     Serial.println("Failed to mount SD Card!");
     while (1) ;
   }
+  Serial.println("SD card initialized.");
+  Serial.printf("Recording %d seconds of audio data...\n", RECORD_TIME);
 
+  wav_buffer = i2s.recordWAV(RECORD_TIME, &wav_size); // record and save raw file data to buffer
 
-  //SD.mkdir(myFolderName,0555);  // read and write access without run access for all levels
-  //SD.mkdir(myFolderName);  // read and write access without run access for all levels
-  createDir(SD, myFolderName);
-  record_wav();
-}
+  // Create a file on the SD card
+  File file = SD.open(("/" + String(FILE_NAME)).c_str(), FILE_WRITE);
+  if (!file) {
+    Serial.printf("Unable to open %s for writing.\n", FILE_NAME);
+    return;
+  }
 
-void createDir(fs::FS &fs, const char * path){
-    Serial.printf("Creating Dir: %s\n", path);
-    if(fs.mkdir(path)){
-        Serial.println("Dir created");
-    } else {
-        Serial.println("mkdir failed");
-    }
+  Serial.println("Writing audio data to file...");
+
+  // Write the audio data to the file
+  if (file.write(wav_buffer, wav_size) != wav_size) {
+    Serial.println("Failed to write audio data to file!");
+    return;
+  }
+
+  // Close the file
+  file.close();
+
+  Serial.println("Program completed without error");
 }
 
 void loop() {
-  delay(10000);
-  //Serial.printf(".");
-  record_wav();
-}
-
-void record_wav()
-{
-  myCount += 1;
-  digitalWrite(LED_BUILTIN, LOW);
-  uint32_t sample_size = 0;
-  uint32_t record_size = (SAMPLE_RATE * SAMPLE_BITS / 8) * RECORD_TIME;
-  uint8_t *rec_buffer = NULL;
-  Serial.printf("Ready to start recording ...\n");
-
-
- 
-
-
-  sprintf(myWavFileName, "%s/my-%07d.wav", myFolderName, myCount); 
- 
-
-  File file = SD.open(myWavFileName, FILE_WRITE);
- 
-  // Write the header to the WAV file
-  uint8_t wav_header[WAV_HEADER_SIZE];
-  generate_wav_header(wav_header, record_size, SAMPLE_RATE);
-  file.write(wav_header, WAV_HEADER_SIZE);
-
-  // PSRAM malloc for recording
-  rec_buffer = (uint8_t *)ps_malloc(record_size);
-  if (rec_buffer == NULL) {
-    Serial.printf("malloc failed!\n");
-    while(1) ;
-  }
-  Serial.printf("Buffer: %d bytes\n", ESP.getPsramSize() - ESP.getFreePsram());
-
-  // Start recording
-  esp_i2s::i2s_read(esp_i2s::I2S_NUM_0, rec_buffer, record_size, &sample_size, portMAX_DELAY);
-  if (sample_size == 0) {
-    Serial.printf("Record Failed!\n");
-  } else {
-    Serial.printf("Record %d bytes\n", sample_size);
-  }
-
-  // Increase volume
-  for (uint32_t i = 0; i < sample_size; i += SAMPLE_BITS/8) {
-    (*(uint16_t *)(rec_buffer+i)) <<= VOLUME_GAIN;
-  }
-
-  // Write data to the WAV file
-  Serial.printf("Writing to the file ...\n");
-  if (file.write(rec_buffer, record_size) != record_size)
-    Serial.printf("Write file Failed!\n");
-
-  free(rec_buffer);
-  file.close();
-  Serial.printf("The recording is over. Saved to file: ");
-  Serial.printf(myWavFileName);
-  Serial.printf("\n");
-  digitalWrite(LED_BUILTIN, HIGH);
-}
-
-void generate_wav_header(uint8_t *wav_header, uint32_t wav_size, uint32_t sample_rate)
-{
-  // See this for reference: http://soundfile.sapp.org/doc/WaveFormat/
-  uint32_t file_size = wav_size + WAV_HEADER_SIZE - 8;
-  uint32_t byte_rate = SAMPLE_RATE * SAMPLE_BITS / 8;
-  const uint8_t set_wav_header[] = {
-    'R', 'I', 'F', 'F', // ChunkID
-    file_size, file_size >> 8, file_size >> 16, file_size >> 24, // ChunkSize
-    'W', 'A', 'V', 'E', // Format
-    'f', 'm', 't', ' ', // Subchunk1ID
-    0x10, 0x00, 0x00, 0x00, // Subchunk1Size (16 for PCM)
-    0x01, 0x00, // AudioFormat (1 for PCM)
-    0x01, 0x00, // NumChannels (1 channel)
-    sample_rate, sample_rate >> 8, sample_rate >> 16, sample_rate >> 24, // SampleRate
-    byte_rate, byte_rate >> 8, byte_rate >> 16, byte_rate >> 24, // ByteRate
-    0x02, 0x00, // BlockAlign
-    0x10, 0x00, // BitsPerSample (16 bits)
-    'd', 'a', 't', 'a', // Subchunk2ID
-    wav_size, wav_size >> 8, wav_size >> 16, wav_size >> 24, // Subchunk2Size
-  };
-  memcpy(wav_header, set_wav_header, sizeof(set_wav_header));
+  Serial.printf(".");
+  delay(1000);
 }
